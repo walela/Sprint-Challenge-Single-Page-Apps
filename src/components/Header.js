545cebecb2fa858207f6ebf6a7d6fc03e96e6c11
Austin Walela <austinmasibo@gmail.com>
2019-11-15 13:32:57 +0300

    added NavLinks to different tabs in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { Menu } from "./styles";
 
 export default function Header() {
   return (
-    <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
-    </header>
+    <React.Fragment>
+      <header className="ui centered">
+        <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      </header>
+
+      <Menu>
+        <div className="menu-container">
+          <NavLink exact to="/" activeClassName="selected">
+            Home Page
+          </NavLink>
+          <NavLink to="/characters" activeClassName="selected">
+            Characters
+          </NavLink>
+          <NavLink to="/locations" activeClassName="selected">
+            Locations
+          </NavLink>
+        </div>
+      </Menu>
+    </React.Fragment>
   );
 }
