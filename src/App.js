6061d14fa4ebf6d6a5305c4cbaaebfcc04f13c31
Austin Walela <austinmasibo@gmail.com>
2019-11-15 13:45:19 +0300

    stretch -- added Location cards for location tab

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import Header from "./components/Header";
 import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
+import LocationsList from "./components/LocationsList";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Header />
       <Route exact path="/" component={WelcomePage} />
       <Route path="/characters" component={CharacterList} />
+      <Route path="/locations" component={LocationsList} />
     </main>
   );
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { Card } from "./styles";
+import { useLocalStorage } from "./CustomHooks";
 
-export default function LocationsList() {}
+export default function LocationsList() {
+  const [locations, setLocations] = useLocalStorage("locations", []);
+
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/location")
+      .then(response => {
+        const newLocations = response.data.results.map(item => {
+          return {
+            id: item.id,
+            name: item.name,
+            type: item.type,
+            dimension: item.dimension,
+            residents: item.residents.length
+          };
+        });
+        setLocations(newLocations);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <section className="character-list grid-view">
+      {locations.map(({ id, name, type, dimension, residents }) => {
+        return (
+          <Card key={id}>
+            <div className="content-container">
+              <h2>{name}</h2>
+              <p className="meta-data">{residents} residents</p>
+              <div className="description">
+                <p>
+                  {type} - {dimension}
+                </p>
+              </div>
+            </div>
+          </Card>
+        );
+      })}
+    </section>
+  );
+}
