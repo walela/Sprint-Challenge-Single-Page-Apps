a7fc583ea5bfb84ca89cb3e1eb24af18cae862d9
Austin Walela <austinmasibo@gmail.com>
2019-11-15 12:07:11 +0300

    created Character card component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,22 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+export default function CharacterCard(props) {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={props.image} />
+      <Card.Body>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Text>Species: {props.species}</Card.Text>
+        <Card.Text>Status: {props.status}</Card.Text>
+        <Card.Text>Gender: {props.gender}</Card.Text>
+        <Card.Text>Location: {props.location.name}</Card.Text>
+        <Button variant="primary">
+          <a href={props.url}>Learn more</a>
+        </Button>
+      </Card.Body>
+    </Card>
+  );
 }
