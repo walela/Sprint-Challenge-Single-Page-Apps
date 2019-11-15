47d30dfc790e0f2839816502d2937595684331d0
Austin Walela <austinmasibo@gmail.com>
2019-11-15 13:49:40 +0300

    tweaked Search Form styles

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,28 @@
 import React from "react";
 
+const inputstyles = {
+  padding: "10px",
+  fontFamily: "Fira Mono, monospace"
+};
+
+const buttonstyles = {
+  fontSize: "1.5em",
+  fontFamily: "monospace"
+};
 export default function SearchForm({ keyword, onSearch, handleInputChange }) {
   return (
     <section className="search-form">
       <form onSubmit={e => onSearch(e, keyword)}>
-        <input onChange={handleInputChange} placeholder="name" value={keyword} name="name" />
-        <button type="submit">Search</button>
+        <input
+          onChange={handleInputChange}
+          placeholder="Search by name..."
+          style={inputstyles}
+          value={keyword}
+          name="name"
+        />
+        <button style={buttonstyles} type="submit">
+          Search
+        </button>
       </form>
     </section>
   );
